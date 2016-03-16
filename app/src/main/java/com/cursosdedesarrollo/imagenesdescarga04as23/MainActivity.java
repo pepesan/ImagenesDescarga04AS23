66e389153a85e28fd62ed22293b5067faef4480b
David Vaquero Santiago <pepesan@gmail.com>
2016-03-16 20:22:37 +0100

    descargar imagen de internet

--- app/src/main/java/com/cursosdedesarrollo/imagenesdescarga04as23/MainActivity.java
+++ app/src/main/java/com/cursosdedesarrollo/imagenesdescarga04as23/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cursosdedesarrollo.imagenesdescarga04as23;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,18 +10,25 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     public static TextView tv;
+    public static ImageView iv;
+    private static String url="http://www.telecinco.es/todoelmundoesbueno/pilar-rubio-estrena-todo-el-mundo-es-bueno_MDSVID20120625_0117_4.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=(TextView)findViewById(R.id.texto_cargando);
-
+        iv=(ImageView)findViewById(R.id.imageView);
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
     }
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             //downloadAction();
 
             MiTarea tarea=new MiTarea();
-            tarea.execute(new Void[]{null});
+            tarea.execute(new String[]{url});
             return true;
         }
         if (id == R.id.action_reset) {
@@ -54,22 +62,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    static public class MiTarea extends AsyncTask<Void,Void,Void> {
+    static public class MiTarea extends AsyncTask<String,Void,Bitmap> {
         protected void onPreExecute(){
             tv.setText("Descargando");
+
             //cargando.setVisibility(View.VISIBLE);
             //descarga.setEnabled(false);
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-
+        protected Bitmap doInBackground(String... params) {
+            /*
             try {
                 Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            */
+            Bitmap bm=getBitmapFromURL(params[0]);
             /*try {
                 downloadBitmap =
                         downloadBitmap(params[0]);
@@ -79,10 +89,11 @@ public class MainActivity extends AppCompatActivity {
             }
             return downloadBitmap;
             */
-            return null;
+            return bm;
         }
-        protected void onPostExecute(Void bm){
+        protected void onPostExecute(Bitmap bm){
             tv.setText("Descargado");
+            iv.setImageBitmap(bm);
             /*
             imageView.setImageBitmap(bm);
             //dialog.dismiss();
@@ -91,5 +102,19 @@ public class MainActivity extends AppCompatActivity {
             */
         }
 
+    }
+    static public Bitmap getBitmapFromURL(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
